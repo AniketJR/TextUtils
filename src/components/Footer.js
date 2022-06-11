286fd6d0c2d00011353faf6266bfa662925d96e4
Aniket Ravikantiwar <71182694+AniketJR@users.noreply.github.com>
2022-06-11 12:45:32 +0530

    Updated Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Footer(props) {
   return (
     <div>
-      <p className={`text-center bg-${props.mode==='dark'?'light': 'dark'} text-${props.mode} mb-0 py-2`}><b>© TextUtils</b></p>
+      <p className={`text-center bg-${props.mode==='dark'?'light': 'dark'} text-${props.mode} mb-0 py-2`}><b>TextUtils | Made with ❤️ by <a href='https://www.linkedin.com/in/aniket-ravikantiwar/' target="_blank">Aniket</a></b></p>
     </div>
   )
 }
